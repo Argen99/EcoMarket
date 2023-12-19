ff896d53aa5b3cabd88bd946f5d09f0b3fadbfe1
Argen99 <argen1999@mail.ru>
2023-12-19 23:49:39 +0600

    6/14.

--- features/main/src/main/java/com/example/main/presentation/ui/fragments/main/MainViewModel.kt
+++ features/main/src/main/java/com/example/main/presentation/ui/fragments/main/MainViewModel.kt
@@ -2,8 +2,10 @@ package com.example.main.presentation.ui.fragments.main
 
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
+import com.example.main.domain.model.ProductCategoryModel
 import com.example.main.domain.repository.MainRepository
 import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.flow.MutableSharedFlow
 import kotlinx.coroutines.launch
 import javax.inject.Inject
 
@@ -11,6 +13,7 @@ import javax.inject.Inject
 class MainViewModel @Inject constructor(
     private val repository: MainRepository
 ): ViewModel() {
+
 
     init {
         viewModelScope.launch {
