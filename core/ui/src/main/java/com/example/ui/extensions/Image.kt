cd9c7f19118686addc001d9c261c024785b09a9d
Argen99 <argen1999@mail.ru>
2023-12-24 23:44:35 +0600

    11/14

--- core/ui/src/main/java/com/example/ui/extensions/Image.kt
+++
@@ -1,7 +0,0 @@
-package com.example.ui.extensions
-
-import android.widget.ImageView
-import com.bumptech.glide.Glide
-
-fun ImageView.loadImageWithGlide(url: String?) =
-    Glide.with(this).load(url).centerCrop().into(this)
